0b4b0bfc32fb9d4145211a4a23520208435b8cb1
RoyNatanaelSaing <natanaelroy123@gmail.com>
2025-12-16 21:37:56 +0700

    Successfully used tanstack (React Query) to retrieve profile table data

--- src/app/(dashboard)/admin/user/page.tsx
+++ src/app/(dashboard)/admin/user/page.tsx
@@ -1,7 +1,9 @@
+import UserManagement from "./_components/user";
+
+export const metadata = {
+  title: "ROY Cafe | User Management",
+};
+
 export default function UserManagementPage() {
-  return (
-    <div>
-      <h1>Pasti Mamah Kamu bangga</h1>
-    </div>
-  );
+  return <UserManagement />;
 }
