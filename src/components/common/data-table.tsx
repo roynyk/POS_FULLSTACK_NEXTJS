cdab60c1f8617293ad2218eb062b2e7cd511fe42
RoyNatanaelSaing <natanaelroy123@gmail.com>
2025-12-17 21:05:31 +0700

    Adding a data table to the admin dashboard (user menuu)

--- src/components/common/data-table.tsx
+++ src/components/common/data-table.tsx
@@ -29,7 +29,6 @@ export default function DataTable({
                   {column}
                 </TableHead>
               ))}
-              ;
             </TableRow>
           </TableHeader>
           <TableBody>
