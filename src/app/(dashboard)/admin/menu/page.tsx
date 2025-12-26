0f37729f8359d29788810e554358003f9f5e7af5
RoyNatanaelSaing <natanaelroy123@gmail.com>
2025-12-26 23:05:07 +0700

    create a dashboard menu (admin)

---
+++ src/app/(dashboard)/admin/menu/page.tsx
@@ -0,0 +1,9 @@
+import MenuManagement from "./_components/menu";
+
+export const metadata = {
+  title: "ROY Cafe | Menu Management",
+};
+
+export default function UserManagementPage() {
+  return <MenuManagement />;
+}
