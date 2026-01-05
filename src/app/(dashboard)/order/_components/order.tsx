427b48071b0a31341dd1c8f46e0f0e23399be85b
RoyNatanaelSaing <natanaelroy123@gmail.com>
2026-01-05 23:50:23 +0700

    (Rolebase Layout)create specific access for specific roles and slightly improve the table status when the order has been settled

--- src/app/(dashboard)/order/_components/order.tsx
+++ src/app/(dashboard)/order/_components/order.tsx
@@ -24,6 +24,7 @@ import DialogCreateOrder from "./dialog-create-order";
 import { updateReservation } from "../actions";
 import { INITIAL_STATE_ACTION } from "@/constants/general-constant";
 import Link from "next/link";
+import { useAuthStore } from "@/stores/auth-store";
 
 export default function OrderManagement() {
   const supabase = createClient();
@@ -35,6 +36,7 @@ export default function OrderManagement() {
     handleChangeLimit,
     handleChangeSearch,
   } = useDataTable();
+  const profile = useAuthStore((state) => state.profile);
 
   const {
     data: orders,
@@ -179,7 +181,7 @@ export default function OrderManagement() {
         </div>,
         <DropdownAction
           menu={
-            order.status === "reserved"
+            order.status === "reserved" && profile.role !== "kitchen"
               ? reservedActionList.map((item) => ({
                   label: item.label,
                   action: () =>
@@ -217,12 +219,14 @@ export default function OrderManagement() {
             placeholder="Search..."
             onChange={(e) => handleChangeSearch(e.target.value)}
           />
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button variant="outline">Create</Button>
-            </DialogTrigger>
-            <DialogCreateOrder tables={tables} refetch={refetch} />
-          </Dialog>
+          {profile.role !== "kitchen" && (
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="outline">Create</Button>
+              </DialogTrigger>
+              <DialogCreateOrder tables={tables} refetch={refetch} />
+            </Dialog>
+          )}
         </div>
       </div>
       <DataTable
